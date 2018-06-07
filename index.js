a1d6c3e30cc3a3eede5465a3be767a95d5b545b2
jack-flux <jflux.61744@farnborough.ac.uk>
2018-06-07 20:10:39 +0100

    Poll trading & 2fa

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-class OPSkinsTrade {
-  constructor(api_key) {
+const EventEmitter = require('events');
+const authenticator = require('authenticator');
+const Request = require('./libs/helpers/request');
+const Item = require('./libs/classes/Item');
+const Trade = require('./libs/classes/Trade');
+const User = require('./libs/classes/User');
+
+const sleep = (ms) => new Promise(resolve => setTimeout(() => resolve(), ms));
+
+class OPSkinsTrade extends EventEmitter {
+  constructor(api_key, secret, polling = 1000) {
+    super();
+    this.request = new Request(api_key);
+    this.Item = new Item(this.request);
+    this.Trade = new Trade(this.request);
+    this.User = new User(this.request);
     this.api_key = api_key;
-    this.request = new (require('./libs/helpers/request'))(api_key);
-    this.Item = new (require('./libs/classes/Item'))(this.request);
-    this.Trade = new (require('./libs/classes/Trade'))(this.request);
-    this.User = new (require('./libs/classes/User'))(this.request);
+    this.secret = secret;
+    this.polling = polling;
+    this.polledTrades = {};
+  }
+
+  async pollTrades() {
+    const offers = (await this.Trade.getOffers()).response.offers;
+    offers.forEach((offer) => {
+      if (!this.polledTrades[offer.id]) {
+        this.polledTrades[offer.id] = offer;
+        this.emit('incoming trade', offer);
+      }
+    });
+    await sleep(this.polling);
+    this.pollTrades();
+  }
+  generateTwoFactor() {
+    return authenticator.generateToken(this.secret);
   }
 }
 
