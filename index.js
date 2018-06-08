7f574f08b8c16d84b3d3b376e74d4003426c6e2d
jack-flux <jflux.61744@farnborough.ac.uk>
2018-06-08 01:02:50 +0100

    Remove polldata

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const User = require('./libs/classes/User');
 const sleep = (ms) => new Promise(resolve => setTimeout(() => resolve(), ms));
 
 class OPSkinsTrade extends EventEmitter {
-  constructor(api_key, secret, pollData = {}, polling = 1000) {
+  constructor(api_key, secret, polling = 1000) {
     super();
     this.request = new Request(api_key);
     this.Item = new Item(this.request);
@@ -16,7 +16,6 @@ class OPSkinsTrade extends EventEmitter {
     this.User = new User(this.request);
     this.api_key = api_key;
     this.secret = secret;
-    this.polledTrades = pollData;
     this.polling = polling;
   }
 
@@ -26,16 +25,10 @@ class OPSkinsTrade extends EventEmitter {
   async pollTrades() {
     const offers = (await this.Trade.getOffers()).response.offers;
     offers.forEach((offer) => {
-      if (!this.polledTrades[offer.id]) {
-        this.polledTrades[offer.id] = offer;
-        this.emit('incoming trade', offer);
-      }
+      this.emit('incoming trade', offer);
     });
     await sleep(this.polling);
     this.pollTrades();
-  }
-  async acceptTrade() {
-    
   }
 }
 
