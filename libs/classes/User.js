947a3a4e0fb7d937262a56db1bcbdcb163911901
jack-flux <jackflux@hotmail.com>
2018-06-27 15:05:14 +0100

    Another endpoint

--- libs/classes/User.js
+++ libs/classes/User.js
@@ -3,6 +3,10 @@ class User {
     this.request = request;
   }
 
+  async createVCaseUser(params) {
+    const user = await this.request.get('IUser/CreateVCaseUser/v1/', params);
+    return user;
+  }
   async getInventory(params) {
     const inventory = await this.request.get('IUser/GetInventory/v1', params);
     return inventory;
